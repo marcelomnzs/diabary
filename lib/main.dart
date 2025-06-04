6a43d9599b755856fdd43165e8206b70af0c3158
Marcelo Antonio <marceloa.mnzs@gmail.com>
2025-06-04 09:37:07 -0300

    Ajustando main.dart para evitar o redirecionamento automático durante a troca de tema

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:diabary/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
@@ -16,33 +17,42 @@ Future<void> main() async {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late final AuthService _authService;
+  late final AuthProvider _authProvider;
+  late final GoRouter _router;
+
+  @override
+  void initState() {
+    super.initState();
+    _authService = AuthService();
+    _authProvider = AuthProvider(_authService);
+    _router = createRouter(_authProvider);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider<AuthService>(create: (_) => AuthService()),
-
-        ChangeNotifierProvider<AuthProvider>(
-          create: (context) => AuthProvider(context.read<AuthService>()),
-        ),
-
+        Provider<AuthService>.value(value: _authService),
+        ChangeNotifierProvider<AuthProvider>.value(value: _authProvider),
         ChangeNotifierProvider<ThemeProvider>(create: (_) => ThemeProvider()),
       ],
-      child: Builder(
-        builder: (context) {
-          final authProvider = context.read<AuthProvider>();
-          final themeProvider = context.watch<ThemeProvider>();
-          final router = createRouter(authProvider);
-
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
           final textTheme = createTextTheme(context, 'Inter', 'Inter');
           final materialTheme = MaterialTheme(textTheme);
 
           return MaterialApp.router(
             debugShowCheckedModeBanner: false,
-            routerConfig: router,
+            routerConfig: _router,
             theme: materialTheme.light(),
             darkTheme: materialTheme.dark(),
             themeMode: themeProvider.themeMode,
