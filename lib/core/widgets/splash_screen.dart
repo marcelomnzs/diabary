ca948f629035ef0ac76b3cbcadf216eb705d1b3d
Marcelo Antonio <marceloa.mnzs@gmail.com>
2025-07-17 21:35:21 -0300

    Melhorias na splash screen

--- lib/core/widgets/splash_screen.dart
+++ lib/core/widgets/splash_screen.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.healing_rounded, size: 70),
+              Text(
+                'Diabary',
+                style: TextStyle(
+                  fontSize: 52,
+                  fontWeight: FontWeight.w900,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              SizedBox(height: 25),
+              SizedBox(
+                height: 40,
+                width: 40,
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).colorScheme.primary,
+                  strokeWidth: 5.8,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
